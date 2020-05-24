816c160c8e5a5d5d6840f5659a08e049347cf774
Joao Vitor <vitor.lira98@gmail.com>
2020-05-24 00:51:59 -0300

    db e entidades

---
+++ src/main/java/br/com/alura/forum/model/Curso.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Curso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String categoria;
+}
---
+++ src/main/java/br/com/alura/forum/model/StatusTopico.java
@@ -0,0 +1,5 @@
+package br.com.alura.forum.model;
+
+public enum StatusTopico {
+	NAO_RESPONDIDO, NAO_SOLUCIONADO, SOLUCIONADO, FECHADO;
+}
