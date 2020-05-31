55eb2f52cc98f85d4cee613965c064575d824013
Joao Vitor <vitor.lira98@gmail.com>
2020-05-31 12:35:22 -0300

    monitorando com spring actuator

--- src/main/java/br/com/alura/forum/config/security/Security.java
+++ src/main/java/br/com/alura/forum/config/security/Security.java
@@ -43,9 +43,10 @@ public class Security extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
 				.antMatchers(HttpMethod.POST, "/auth").permitAll().antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilterBefore(
-						new JwtValidation(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll().anyRequest().authenticated().and().csrf()
+				.disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(new JwtValidation(tokenService, usuarioRepository),
+						UsernamePasswordAuthenticationFilter.class);
 	}
 
 	/*
